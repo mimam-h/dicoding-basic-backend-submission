c84725bf2f37ce19b2f903f5fe53b6f83ef9bc69
Muhammad Imam Hidayat <imamhidayat24@gmail.com>
2023-03-20 14:01:15 +0700

    This commit add feature to fulfill the optional feature

--- src/handler/list.js
+++ src/handler/list.js
@@ -1,8 +1,21 @@
 import books from '../storage/books.js';
 
-const listOfBook = books;
-
 const getAllBook = (request, h) => {
+  let listOfBook = books;
+  const { name, reading, finished } = request.query;
+  if (name !== undefined && reading !== undefined && finished !== undefined) {
+    listOfBook.filter(
+      (b) => b.name.toLowerCase().includes(name.toLowerCase())
+        && b.reading === Boolean(reading)
+        && b.finished === Boolean(finished),
+    );
+  } else if (name !== undefined) {
+    listOfBook = listOfBook.filter((b) => b.name.toLowerCase().includes(name.toLowerCase()));
+  } else if (reading !== undefined) {
+    listOfBook = listOfBook.filter((b) => b.reading === (reading == 1));
+  } else if (finished !== undefined) {
+    listOfBook = listOfBook.filter((b) => b.finished === (finished == 1));
+  }
   const response = h.response({
     status: 'success',
     data: {
